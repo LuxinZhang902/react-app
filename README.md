a1251091716c3ddf92981af60e4808e4b219c8e6
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-11-12 15:54:11 -0500

    Managing stated

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ npm i bootstrap@5.2.3
 ## Handling Events
 
 ## Managing States
+
+1. Using the updater functionwe can update variable and at that point react will be notified
+   so it knonws that the state of our component is changed and then it will re-render our component
+   which causes the Dom to be updated under the hood
+2. In React, we almost never have to touch the Dom directly we think in terms of components that have state
+   when the state of a component changes, react will update the Dom to match the new component State
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,9 @@
-import { Fragment } from "react";
-import { MouseEvent } from "react";
+import { Fragment, useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Franscisco", "Tokyo", "London", "Paris"];
-
-  //An event handler: calling this function would be later at the run time
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //Hook
+  const [selectedIndex, setSelectIndex] = useState(-1);
 
   //In React, a component can not return more than one element
   //In jsx, we can only use HTML elements or other react components
@@ -13,11 +11,20 @@ function ListGroup() {
   return (
     <Fragment>
       <h1>List</h1>
-      {/* {items.length === 0 ? <p>No item found</p> : null} */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
