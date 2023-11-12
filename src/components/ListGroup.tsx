bfe871448de0fdda8d7bf9981f028a5fba6e7bdf
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-11-12 15:30:00 -0500

    using function outside return to do condition

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,6 +3,10 @@ import { Fragment } from "react";
 function ListGroup() {
   let items = ["New York", "San Franscisco", "Tokyo", "London", "Paris"];
   items = [];
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
   //   if (items.length == 0)
   //     return (
   //       <>
@@ -18,7 +22,7 @@ function ListGroup() {
   return (
     <Fragment>
       <h1>List</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
