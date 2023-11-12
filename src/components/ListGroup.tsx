c8431bcde136eba86e86276f273660145ad9b1c8
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-11-12 15:38:19 -0500

    type annotation

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,10 @@
 import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Franscisco", "Tokyo", "London", "Paris"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   //In React, a component can not return more than one element
   //In jsx, we can only use HTML elements or other react components
@@ -12,11 +15,11 @@ function ListGroup() {
       {/* {items.length === 0 ? <p>No item found</p> : null} */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log(item)}
+            onClick={(event) => console.log(event)}
           >
             {item}
           </li>
